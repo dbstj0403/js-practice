86f9b96fd79ce24007a7bbc5295ed6f7b523c9a5
dbstj0403 <wcn09088@naver.com>
2025-04-16 11:15:46 +0900

    Chore: 페이지 이동시 절대경로 링크로 이동

--- constants/festivalData.js
+++ constants/festivalData.js
@@ -1,23 +1,37 @@
-const festivalData = [
+export const festivalData = [
   {
     title: "태안봄꽃정원",
     space: "충남 태안군",
-    startTime: "2025-04-03",
-    endTime: "2025-06-30",
+    startTime: new Date("2025-04-03"),
+    endTime: new Date("2025-06-30"),
     address: "충청남도 태안군 남면 마검포길 200",
   },
   {
     title: "부산국제매직페스티벌",
     space: "부산 동구",
-    startTime: "2025-04-20",
-    endTime: "2025-04-23",
+    startTime: new Date("2025-04-20"),
+    endTime: new Date("2025-04-23"),
     address: "부산광역시 동구 자성로 133번길 16 (범일동)",
   },
   {
     title: "대전시립연정국악단 토요국악",
     space: "대전 서구",
-    startTime: "2025-04-22",
-    endTime: "2025-04-25",
+    startTime: new Date("2025-04-22"),
+    endTime: new Date("2025-04-25"),
     address: "충청남도 태안군 남면 마검포길 200",
+  },
+  {
+    title: "용인 자작나무숲 봄꽃축제",
+    space: "경기 용인시",
+    startTime: new Date("2025-04-02"),
+    endTime: new Date("2025-06-09"),
+    address: "경기도 용인시 처인구 백암면 석천리 723-2",
+  },
+  {
+    title: "서울봄꽃축제",
+    space: "서울 중구",
+    startTime: new Date("2025-04-10"),
+    endTime: new Date("2025-04-20"),
+    address: "서울특별시 중구 세종대로 110",
   },
 ];
